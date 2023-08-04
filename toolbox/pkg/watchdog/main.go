cd534ebf9f7bea3daca8945a6018014cf085acf3
muhe <muhe@kerrylan.com>
2023-08-04 17:48:47 +0800

    更新

--- toolbox/pkg/watchdog/main.go
+++ toolbox/pkg/watchdog/main.go
@@ -11,7 +11,7 @@ import (
 
 func Save(ctx context.Context, svc *svc.ServiceContext, param []LogInfo, w http.ResponseWriter, r *http.Request) {
 	for _, v := range param {
-		var actionUser = "golang"
+		var actionUser = "golang-zero"
 		if len(v.ActionUser) != common.Zero {
 			actionUser = v.ActionUser
 		}
