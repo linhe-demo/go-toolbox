a16272770d576b80d089096020e48b69da64fba8
muhe <muhe@kerrylan.com>
2024-01-05 16:01:46 +0800

    更新

--- toolbox/pkg/watchdog/main.go
+++ toolbox/pkg/watchdog/main.go
@@ -3,7 +3,6 @@ package watchdog
 import (
 	"context"
 	"net/http"
-	"time"
 	"toolbox/common"
 	"toolbox/internal/models"
 	"toolbox/internal/svc"
@@ -30,7 +29,6 @@ func Save(ctx context.Context, svc *svc.ServiceContext, param []LogInfo, w http.
 			Ip:         ip,
 			Action:     v.Action,
 			ActionUser: actionUser,
-			CreateTime: time.Now(),
 		}
 
 		_, _ = svc.UserLogModel.Insert(ctx, log)
